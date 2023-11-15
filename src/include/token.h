adbe2078cf8bc80b8ecfc4949bc85e3df862ae6d
Marcus Chan <24224821+marcus-hao@users.noreply.github.com>
2023-11-15 09:56:34 +0800

    documentation: token

--- src/include/token.h
+++ src/include/token.h
@@ -8,28 +8,28 @@
 typedef struct TOKEN_STRUCT {
     char* value;
     enum {
-        TOKEN_ID,
-        TOKEN_NUM,
+        TOKEN_ID,       /* Identifier */
+        TOKEN_NUM,      /* Number*/
         TOKEN_IF,
-        TOKEN_COMMA,
-        TOKEN_NOTEQ,
-        TOKEN_DEFINE,
-        TOKEN_LPAREN,
-        TOKEN_RPAREN,
-        TOKEN_ADD,
-        TOKEN_SUB,
-        TOKEN_MUL,
-        TOKEN_DIV,
-        TOKEN_SEMI,
-        TOKEN_PRINT,
-        TOKEN_EOF
+        TOKEN_COMMA,    /* , */
+        TOKEN_NOTEQ,    /* != */
+        TOKEN_DEFINE,   /* := */
+        TOKEN_LPAREN,   /* ( */
+        TOKEN_RPAREN,   /* ) */
+        TOKEN_ADD,      /* + */
+        TOKEN_SUB,      /* - */
+        TOKEN_MUL,      /* * */
+        TOKEN_DIV,      /* / */
+        TOKEN_SEMI,     /* ; */
+        TOKEN_PRINT,    /* print */
+        TOKEN_EOF       /* EOF */
     } type;
 } token_T;
 
-token_T* init_token(char* value, int type);
+token_T* init_token(char* value, int type);     /* Initializes the token */
 
-const char* token_type_to_str(int type);
+const char* token_type_to_str(int type);        /* Returns a string for the token type */
 
-char* token_to_str(token_T* token);
+char* token_to_str(token_T* token);             /* Returns a string for the token information */
 
 #endif
